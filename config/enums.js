c5f07ab2909c09c209b23c30b5eb13edabe23240
velrino <denismagalhaesvelrino@gmail.com>
2019-07-07 00:54:11 -0300

    ENUMS for improved project

---
+++ config/enums.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const ENUMS = {
+    hierarchy: {
+        admin: 3,
+        client: 1,
+        user: 2,
+    },
+    orderStatus: {
+        accepted: 2,
+        canceled: 6,
+        completed: 5,
+        delivery: 4,
+        expired: 7,
+        loading: 3,
+        pending: 1,
+    },
+    orderStatus: {
+        acive: 1,
+        canceled: 6,
+        completed: 5,
+        delivery: 4,
+        expired: 7,
+        loading: 3,
+        pending: 1,
+    }
+}
+
+module.exports = ENUMS
