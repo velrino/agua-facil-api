0bee53ca27cfc9120678141c2aa07204473ceb89
velrino <denismagalhaesvelrino@gmail.com>
2019-07-07 00:54:35 -0300

    Models

---
+++ app/Models/CompaniesPlace.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class CompaniesPlace extends Model {
+}
+
+module.exports = CompaniesPlace
