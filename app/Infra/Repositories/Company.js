c3bf6a776cf1f32c472ab57f12533462523ac270
velrino <denismagalhaesvelrino@gmail.com>
2019-07-09 18:14:38 -0300

    Repo and Model default

--- app/Infra/Repositories/Company.js
+++ app/Infra/Repositories/Company.js
@@ -9,16 +9,17 @@ class CompanyRepository extends DefaultRepository {
     }
 
     async getWhereRawJsonExtract(params = {}) {
-        const query = this.getPayments(params['payments']);
+        let query = this.getPayments(params['payments']);
 
-        return await this.queryWhereRaw(Company.query(), query, 'whereRaw').paginate(1, 10);
+        return await this.queryWhereRaw(Company.query(), query).paginate(1, 10);
     }
 
     getPayments(param = '') {
+        //{"price": "50", "period": [1, 3], "trucks": 4, "payment": [1, 2, 3], "distance": 2}
         const payments = param.split(",");
         let query = [];
         for (let index = 0; index < payments.length; index++) {
-            query.push(this.rawJsonExtract('testCarai', payments[index]));
+            query.push(this.rawJsonExtract('payment', payments[index]));
         }
         return query;
     }
