05f5cda000e4c9852f574ae7e4fcf9472959bd2c
velrino <denismagalhaesvelrino@gmail.com>
2019-09-08 19:09:50 -0300

    Status in companies

--- app/Infra/Repositories/Company.js
+++ app/Infra/Repositories/Company.js
@@ -4,46 +4,46 @@ const DefaultRepository = use('./Default');
 const Company = use('App/Models/Company');
 
 class CompanyRepository extends DefaultRepository {
-  async store(params) {
-    return await Company.create(params);
-  }
-
-  async update(id, params) {
-    return await Company.query().where('id', id).update(params);
-  }
-
-  async get() {
-    return await Company.query().paginate(1, 10);
-  }
-
-  async first(id) {
-    return await Company.query().where('id', id).first();
-  }
-
-  async getWhereRawJsonExtract(params = {}) {
-    let query = this.whereByName(params['name']);
-
-    return await this.queryWhereRaw(Company.query(), query).paginate(1, 10);
-  }
-
-  whereByName(param = null, query = []) {
-    if (param != null) {
-      query.push(this.rawJsonExtract('name_social', param));
-      query.push(this.rawJsonExtract('name_fantasy', param));
+    async store(params) {
+        return await Company.create(params);
     }
 
-    return query;
-  }
-
-  getPayments(param = '') {
-    //{"price": "50", "period": [1, 3], "trucks": 4, "payment": [1, 2, 3], "distance": 2}
-    const payments = param.split(",");
-
-    for (let index = 0; index < payments.length; index++) {
-      query.push(this.rawJsonExtract('name_fantasy', payments[index]));
+    async update(id, params) {
+        return await Company.query().where('id', id).update(params);
     }
-    return query;
-  }
+
+    async get() {
+        return await Company.query().paginate(1, 10);
+    }
+
+    async first(id) {
+        return await Company.query().where('id', id).first();
+    }
+
+    async getWhereRawJsonExtract(params = {}) {
+        let query = this.whereByName(params['name']);
+
+        return await this.queryWhereRaw(Company.query(), query).with('status').paginate(1, 10);
+    }
+
+    whereByName(param = null, query = []) {
+        if (param != null) {
+            query.push(this.rawJsonExtract('name_social', param));
+            query.push(this.rawJsonExtract('name_fantasy', param));
+        }
+
+        return query;
+    }
+
+    getPayments(param = '') {
+        //{"price": "50", "period": [1, 3], "trucks": 4, "payment": [1, 2, 3], "distance": 2}
+        const payments = param.split(",");
+
+        for (let index = 0; index < payments.length; index++) {
+            query.push(this.rawJsonExtract('name_fantasy', payments[index]));
+        }
+        return query;
+    }
 }
 
 module.exports = CompanyRepository
